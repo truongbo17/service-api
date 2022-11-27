3fb8f1d57cbc10bb9d868061427d3520da1b6d91
Nguyen Quang Truong <truongnq017@gmail.com>
2022-11-27 20:41:01 +0700

    wip update doc and add proxy rotation

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 _(Working with PHP8.1)_
 
+- **Importation** Set .env `AUTH_TOKEN_API=your_token` this code will authenticate api requests, only this one will work.
+
+---------------
+### Proxy Rotation
+
+- Github source package : https://github.com/truongbo17/proxy-rotator
+- Set .env `USE_PROXY_ROTATION=true` if you want to use a revolving proxy when making requests to the api.
+- Setup proxy in App/Providers/AppServiceProvider.php method handleProxy().
+
 ----------------
 ### Signature Tiktok Service
 - Github source : https://github.com/pablouser1/SignTok
@@ -11,4 +20,3 @@ docker pull ghcr.io/pablouser1/signtok:master
 docker run --publish 8080:8080 ghcr.io/pablouser1/signtok:master
 ```
 - Set .env `ENDPOINT_TIKTOK_SIGNATURE=http://127.0.0.1:8080`
-
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,13 @@ namespace App\Providers;
 
 use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 use Illuminate\Support\ServiceProvider;
+use TruongBo\ProxyRotation\Middleware\ProxyMiddleware;
+use TruongBo\ProxyRotation\ProxyServer\ProxyCluster;
+use TruongBo\ProxyRotation\ProxyServer\ProxyNode;
+use TruongBo\ProxyRotation\Rotation;
+use TruongBo\ProxyRotation\Strategy\RoundRobin;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +25,30 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(IdeHelperServiceProvider::class);
         }
 
-        $this->app->singleton('client', fn() => new Client(config('crawl.guzzle')));
+        $config = config('crawl.guzzle');
+        if (env('USE_PROXY_ROTATION')) {
+            $config['handler'] = $this->handleProxy();
+        }
+
+        $this->app->singleton('client', fn() => new Client($config));
+    }
+
+    private function handleProxy(): HandlerStack
+    {
+        $rotation = new Rotation(new RoundRobin(counter: 0));
+
+        $proxy_cluster = new ProxyCluster(
+            cluster_name: 'cluster1',
+            array_proxy_node: [
+                new ProxyNode(name: 'proxy-node1'),
+                new ProxyNode(name: 'proxy-node2'),
+                new ProxyNode(name: 'proxy-node3'),
+                new ProxyNode(name: 'proxy-node4'),
+            ]);
+
+        $stack = HandlerStack::create();
+        $stack->push(new ProxyMiddleware(rotation: $rotation, proxy_cluster: $proxy_cluster));
+        return $stack;
     }
 
     /**
